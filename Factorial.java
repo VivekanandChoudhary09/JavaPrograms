a6b72f112334fa30e81cfe53c24887f4192a1578
VivekanandChoudhary09 <choudharyvivekanand80@gmail.com>
2023-12-19 09:05:21 +0530

    firstfile

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial{
+	public static void main(String[] args){
+		int num = 5;
+		long res = 1;
+		for(int i=1; i<=num ; i++){
+			res*=num;
+			
+		}
+			System.out.print(res);
+
+
+	}
+}
